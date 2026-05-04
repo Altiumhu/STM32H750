0e4fbab0220944729962632dde23aa67f6bb8c16
Altium <279894340@qq.com>
2026-05-04 22:04:27 +0800

    备份代码

--- App_API_user/AppUser_Sample.c
+++ App_API_user/AppUser_Sample.c
@@ -551,7 +551,7 @@ void AppUser_ChannelInfo_Debug(void)
     /* 电容电压 */
     printf("电容端口信息 =%f ADC1_A10=%d\r\n", g_Channelinfo[1].Cap_voltage, adc_values[12]);
 
-    for (ch = 12; ch < 16; ch++)
+    for (ch = 0; ch < 5; ch++)
     {
         printf("\r\n ch=[%d] workMode=%d \r\n", ch + 1, g_Channelinfo[ch].workMode);
         printf(" ch=[%d] voltage=%f--ADC=%d current=%f--ADC%d Cap_voltage=%f voltage_port=%f\r\n", ch + 1, g_Channelinfo[ch].voltage, g_Channelinfo[ch].voltage_ADC,
